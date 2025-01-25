1d41387374fb85e5fc0326658e43e280fbbfb52c
Janit07 <janit950@gmail.com>
2025-01-25 03:52:04 -0500

    JS Update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,27 +1,37 @@
 const btnSearch=document.getElementById("btnSearch");
 const btnClear=document.getElementById("btnClear");
 
+let json = {};
+
 function searchDestination() {
             const input = document.getElementById('destination').value.toLowerCase();
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML="";
+            console.log(input);
 
             fetch('travel_recommendation_api.json')
                 .then(response => response.json())
                 .then(data =>{
-                    console.log(data);
-                    const countries = data.countries.find(item => item.name.toLowerCase() === input);
-                    const temples = data.temples.find(item => item.name.toLowerCase() === input);
-                    const beaches = data.beaches.find(item => item.name.toLowerCase() === input);
-
-                    console.log(countries);
-                    console.log(temples);
-                    console.log(beaches);
+                    json=data;
+                    console.log(data);                    
                 })
                 .catch(error =>{
                     console.error('Error:',error);
                     resultDiv.innerHTML ="An Error occurred while fetching data.";
                 });
+            if (input) {
+                        if (json[input]) {
+                                const dataArray = json[input];
+                                dataArray.forEach(item =>{
+                                            resultDiv.innerHTML += `<img src="${item.imageUrl}" alt="hjh">`;
+                                            resultDiv.innerHTML += `<p><strong>${item.name}</strong></p>`;
+
+                                            resultDiv.innerHTML += `<p><strong></strong> ${item.description}</p>`;
+                                })
+                        }
+            } else {
+                        alert('Please enter a keyword to search.');
+            }
                
 }
 btnSearch.addEventListener('click',searchDestination);
